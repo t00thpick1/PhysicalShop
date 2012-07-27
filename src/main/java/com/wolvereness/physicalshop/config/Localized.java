52e9f8df03d43fa4b454d0a7a069e46a9c8296f8
Wesley Wolfe <weswolf@aol.com>
2012-07-27 16:29:26 -0500

    Added support for randomized message feedback. Fixed new colors.
    To use this new feature, just use the yml list notation with []s or the
    dashes for each element. It will randomly select one from the list with
    equal weight per item.

--- src/main/java/com/wolvereness/physicalshop/config/Localized.java
+++ src/main/java/com/wolvereness/physicalshop/config/Localized.java
@@ -2,15 +2,20 @@ package com.wolvereness.physicalshop.config;
 
 import static com.wolvereness.physicalshop.config.ConfigOptions.LANGUAGE;
 import static java.util.logging.Level.SEVERE;
+import static org.bukkit.ChatColor.translateAlternateColorCodes;
 import static org.bukkit.configuration.file.YamlConfiguration.loadConfiguration;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.Validate;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -23,6 +28,7 @@ import com.google.common.io.ByteStreams;
  * Licensed under GNU GPL v3
  */
 public class Localized {
+
 	/**
 	 * @author Wolfe
 	 * Represents all the messages that can be sent
@@ -100,16 +106,30 @@ public class Localized {
 		 */
 		STATUS_ONE_MATERIAL
 	}
+
 	/**
 	 * Regex to find the & symbols to be replaced
+	 * @deprecated Use {@link org.bukkit.ChatColor#translateAlternateColorCodes(char, String)}
 	 */
+	@Deprecated
 	public static Pattern colorReplace = Pattern.compile("&(?=[0-9a-f])");
 	private final YamlConfiguration config;
 	private final Logger logger;
+	private final Random random;
+
 	/**
 	 * @param plugin plugin to consider for getting resources
 	 */
 	public Localized(final Plugin plugin) {
+		this(plugin, new Random());
+	}
+
+	/**
+	 * @param plugin plugin to consider for getting resources
+	 * @param random a random number generator, if messages have multiple outputs
+	 */
+	public Localized(final Plugin plugin, final Random random) {
+		this.random = random;
 		this.logger = plugin.getLogger();
 		final String language = String.valueOf(plugin.getConfig().get(LANGUAGE)).toUpperCase();
 		final File file = new File(plugin.getDataFolder(),"Locales" + File.separatorChar +  language + ".yml");
@@ -143,14 +163,32 @@ public class Localized {
 			plugin.getLogger().log(Level.WARNING, "Failed to save locale file " + file, e);
 		}
 	}
+
 	/**
 	 * @param message message to get
 	 * @return message stored
 	 */
 	public String getMessage(final Message message) {
-		final Object string = config.get(message.name());
-		return string == null ? null : colorReplace.matcher(String.valueOf(string)).replaceAll("\u00A7");
+		final String string = toFormattableMessage(message);
+		return string == null ? null : translateAlternateColorCodes('&', string);
 	}
+
+	/**
+	 * This recursion will only be a problem with nested values,
+	 * where an infinite loop is worse than stack overflow.
+	 */
+	private String parseObject(final Object obj) {
+		if (obj == null)
+			return null;
+		if (obj.getClass().isArray())
+			return parseObject(Array.get(obj, (int) (random.nextDouble() * Array.getLength(obj))));
+		if (obj instanceof List) {
+			final List<?> list = (List<?>) obj;
+			return parseObject(list.get((int) (random.nextDouble() * list.size())));
+		}
+		return obj.toString();
+	}
+
 	/**
 	 * Sends the recipient a formatted message located at the node defined
 	 * @param recipient Player to receive the message
@@ -158,12 +196,17 @@ public class Localized {
 	 * @param args The arguments to String.format
 	 */
 	public void sendMessage(final CommandSender recipient, final Message message, final Object...args) {
-		final Object string = config.get(message.name());
-		if(string == null) {
+		final String string = toFormattableMessage(message);
+		if (string == null) {
 			recipient.sendMessage("ERROR_"+message);
 			logger.log(SEVERE,"Unknown message:" + message + " name:" + message.name(), new Exception());
 		} else {
-			recipient.sendMessage(colorReplace.matcher(String.format(String.valueOf(string), args)).replaceAll("\u00A7"));
+			recipient.sendMessage(translateAlternateColorCodes('&', String.format(string, args)));
 		}
+	}
+
+	private String toFormattableMessage(final Message message) {
+		Validate.notNull(message, "Cannot retrieve message for null");
+		return parseObject(config.get(message.name()));
 	}
 }
